2e79fdee8b2eb2168686125904e32bf923d079a2
Mihail Danilov <mihaildanilov793@gmail.com>
2023-10-19 10:18:47 +0300

    Typo fix in auth.config.js

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -3,7 +3,7 @@ export default {
   providers: [
     {
       domain: process.env.CLERK_ISSUER_URL,
-      applicationID: env.CLERK_APP_ID,
+      applicationID: process.env.CLERK_APP_ID,
     },
   ],
 };
